b5ed55c449ba66b76472a103d6b7010069608720
kbs4674 <kbs4674@naver.com>
2019-08-09 21:56:30 +0000

    [Step 13, 추가 과정 끝] whenever 자동작업 설정

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,22 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+## ↓ 해당 설정을 안해주면, 자동작업 시간은 영국시간 기준을 따르며 일하게 됨.
+require "tzinfo"
+ 
+# 자동작업 시계(Clock)를 '대한민국' 시간으로 맞추는 작업
+def local(time)
+    TZInfo::Timezone.get('Asia/Seoul').local_to_utc(Time.parse(time))
+end
+## ↑ 대한민국 시간 설정 종료
+
+# 매 1분마다 [rake naver:market] 명령어 입력을 통해 해당 파일 내 코드가 작동되도록 설정
+every 1.minutes do
+    rake "naver:market", :environment => "development"
+    rake "naver:market", :environment => "production"
+end
+
+
+### whenever 자세한 사용법 참고 ###
+# https://blog.naver.com/kbs4674/221197430824
